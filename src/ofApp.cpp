fff0836f2e9942861f78b4fbd78f45745bdaa954
ReallyRad <arthur.pointeau@gmail.com>
2016-07-13 14:39:40 +0200

    initial commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,87 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	//some path, may be absolute or relative to bin/data
+	string path = ".";
+
+	ofDirectory dir(path);
+	//only show mov files
+	dir.allowExt("mov");
+	//populate the directory object
+	dir.listDir();
+
+	nr_videos = dir.size();
+
+	stringstream files;
+
+	//go through and load all the videos
+	for (int i = 0; i < dir.size(); i++) {
+		addNewVideo(dir.getPath(i));
+	}
+
+	ofLogNotice(filesPaths);
+
+	current = 0;
+	videoPlayers[0].play();
+
+	fileTimer = ofGetElapsedTimeMillis();
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	if (ofGetElapsedTimeMillis() - fileTimer > 1000) {
+		checkNewVideos();
+		fileTimer = ofGetElapsedTimeMillis();
+	}
+
+	//update current playing video
+	videoPlayers[current].update();
+
+	//if video finished, rewind, pause and go to next video
+	if (videoPlayers[current].getIsMovieDone()) {
+		videoPlayers[current].stop();
+		//videoPlayers[current].setFrame(0);
+		if (current < videoPlayers.size() - 1) current++;
+		else current = 0;
+		videoPlayers[current].play();
+	}
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+	//draw current video
+	videoPlayers[current].draw(ofGetWidth() / 2, ofGetHeight() / 2);
+}
+
+void ofApp::checkNewVideos() {
+
+	ofDirectory dir(".");
+	dir.listDir();
+
+	if (dir.size() > nr_videos) nr_videos = dir.size();
+
+	//look for new files in folder
+	for (int i = 0; i < dir.size(); i++) {
+		for (int j = 0; j < filesPaths.size(); j++) {
+			//if video not already in files list, add it
+			if (std::find(filesPaths.begin(), filesPaths.end(), dir.getPath(i)) == filesPaths.end()) {
+				cout << "new video found" << endl;
+				addNewVideo(dir.getPath(i));
+			}
+		}
+	}
+
+}
+
+void ofApp::addNewVideo(string path) {
+	filesPaths.push_back(path);
+	ofVideoPlayer * v = new ofVideoPlayer();
+	v->load(path);
+	v->setLoopState(OF_LOOP_NONE);
+	v->setAnchorPercent(0.5, 0.5);
+	videoPlayers.push_back(*v);
+}
---
+++ src/ofApp.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp {
+
+public:
+	void setup();
+	void update();
+	void draw();
+	void checkNewVideos();
+	void addNewVideo(string path);
+
+	std::vector<ofVideoPlayer> videoPlayers;
+	int current;
+	int nr_videos;
+	std::vector<string> filesPaths;
+	double fileTimer;
+};
